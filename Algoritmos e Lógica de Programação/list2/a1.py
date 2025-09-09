19a3801f6a3750160c8fb8a0c9533df9c5f04889
Thiago Ribeiro <167592184+thiagoConsta@users.noreply.github.com>
2025-09-09 10:51:53 -0300

    Exercise of P.A
    Today we learn a repeat loop in Python

---
+++ Algoritmos e Lógica de Programação/list2/a1.py
@@ -0,0 +1,10 @@
+#1. Faça um algoritmo que leia dois números e apresente-os em ordem crescente. 
+# Use estrutura de desvio condicional simples.
+print("Apresente dois números pra saber a ordem crescente")
+n1 = int(input("Digite o primeiro número:"))
+n2 = int(input("Digite o segundo número:"))
+
+if n1 >= n2:
+    print("A ordem crescente é: ",n1,n2)
+else:
+    print("A ordem crescente é: ",n2,n1)
---
+++ Algoritmos e Lógica de Programação/list2/dia-da-semana.py
@@ -0,0 +1,18 @@
+dia = int (input("Coloque o dia da semana em número ai amigao: "))
+
+if dia == 1:
+    print("Domingo")
+elif dia == 2:
+    print("Segunda")
+elif dia == 3:
+    print("Terça")
+elif dia == 4:
+    print("Quarta")
+elif dia == 5:
+    print("Quinta")
+elif dia == 6:
+    print("Sexta")
+elif dia == 7:
+    print("Sábado")
+else:
+    print("Número inválido caramba.")
